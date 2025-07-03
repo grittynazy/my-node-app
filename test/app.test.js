2dcdbb2726c86caeda57276d19cc36c250d7e551
Gertrude <grittynazy@gmail.com>
2025-07-03 13:08:25 +0100

    ok

--- test/app.test.js
+++ test/app.test.js
@@ -1,18 +1,17 @@
-// test/app.test.js
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../index'); // If you're exporting app from index.js
+const app = require('../index');
 
 const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('GET /', () => {
-  it('should return Hello World!', (done) => {
+  it('should return HTML with Hello, AWS EC2!', (done) => {
     chai.request(app)
       .get('/')
       .end((err, res) => {
         expect(res).to.have.status(200);
-        expect(res.text).to.equal('Hello World!');
+        expect(res.text).to.include('Hello, AWS EC2!');
         done();
       });
   });
